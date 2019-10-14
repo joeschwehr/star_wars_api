6c3fdb28f0c68bbb44e360e0a77b09809dec9423
Joe <joeschwehr@gmail.com>
2019-10-14 13:12:45 -0700

    Updates

---
+++ precache-manifest.b3fa9ca4d64043bdeb914aa251121806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913dda44bbb9ed37589e23accb68fa05",
+    "url": "/joeschwehr/star_wars_api/index.html"
+  },
+  {
+    "revision": "398abc1d15e4eea1a590",
+    "url": "/joeschwehr/star_wars_api/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "28d745b7d43e0f6d849c",
+    "url": "/joeschwehr/star_wars_api/static/css/main.0f572332.chunk.css"
+  },
+  {
+    "revision": "398abc1d15e4eea1a590",
+    "url": "/joeschwehr/star_wars_api/static/js/2.391a59ea.chunk.js"
+  },
+  {
+    "revision": "28d745b7d43e0f6d849c",
+    "url": "/joeschwehr/star_wars_api/static/js/main.bf6abb38.chunk.js"
+  },
+  {
+    "revision": "52af744bb9a5bcc99481",
+    "url": "/joeschwehr/star_wars_api/static/js/runtime-main.fda29406.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/joeschwehr/star_wars_api/static/media/SEGA.12a3db8b.woff"
+  }
+]);
